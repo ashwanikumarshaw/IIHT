5f48b9953543ad1d79720a7a32cdf2c615c184a3
unknown <cogjava4342@iiht.tech>
2022-11-08 18:36:52 +0530

    localdate

---
+++ OOPs-basic/src/com/date/DemoDate.java
@@ -0,0 +1,15 @@
+package com.date;
+
+import java.time.LocalDate;
+
+public class DemoDate {
+	public static void main(String[] args) {
+		LocalDate date=LocalDate.now();
+		LocalDate yesterday=date.minusDays(1);
+		LocalDate tomorrow =date.plusDays(1);
+		
+		System.out.println(date);
+		System.out.println(yesterday);
+		System.out.println(tomorrow);
+	}
+}
