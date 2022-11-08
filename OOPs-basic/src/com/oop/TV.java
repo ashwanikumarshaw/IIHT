8982c56ef823400877599d4aa25e861fd4b89518
unknown <cogjava4342@iiht.tech>
2022-11-08 14:36:12 +0530

    inheritance

--- OOPs-basic/src/com/oop/TV.java
+++ OOPs-basic/src/com/oop/TV.java
@@ -1,15 +1,15 @@
 package com.oop;
 
 public class TV {
-	static String brand;
-	static String modelname;
-	static  Integer screensize;
-	static Integer price;
-	 static Integer color;
-	 static String displaytype;
-	
-	static void insertData(String b, String m, Integer s, Integer p, Integer c, String d) {
-		
+	String brand;
+	String modelname;
+	Integer screensize;
+	Integer price;
+	Integer color;
+	String displaytype;
+
+	void insertData(String b, String m, Integer s, Integer p, Integer c, String d) {
+
 		brand = b;
 		modelname = m;
 		screensize = s;
@@ -17,21 +17,25 @@ public class TV {
 		color = c;
 		displaytype = d;
 	}
-	static void displayTV() {  System.out.println(brand+" "+modelname);}
+
+	void displayTV() {
+		System.out.println(brand + " " + modelname);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  TV lg=new TV();
-		  lg.brand="LG";
-		  lg.modelname="4k oled";
-		  lg.screensize=55;
-		  lg.price=60000;
-		  lg.displaytype="Oled";
-		  System.out.println(lg.brand+" "+lg.modelname);
-		  displayTV();
-		  insertData("Samsung","LO12",52,87897,5,"LED");
-		  displayTV();
-		  
+		TV lg = new TV();
+		lg.brand = "LG";
+		lg.modelname = "4k oled";
+		lg.screensize = 55;
+		lg.price = 60000;
+		lg.displaytype = "Oled";
+		System.out.println(lg.brand + " " + lg.modelname);
+
+		TV samsung = new TV();
+		samsung.displayTV();
+		samsung.insertData("Samsung", "LO12", 52, 87897, 5, "LED");
+		samsung.displayTV();
 
 	}
 
