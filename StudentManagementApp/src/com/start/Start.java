295a68e5ba7d512f434f4b638f86e5bb7d7763bb
unknown <cogjava4342@iiht.tech>
2022-11-09 16:18:34 +0530

    StudentManagement add delete dispaly

--- StudentManagementApp/src/com/start/Start.java
+++ StudentManagementApp/src/com/start/Start.java
@@ -3,13 +3,15 @@ package com.start;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.start.student.Student;
 import com.start.student.StudentDao;
 
 public class Start {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLException {
 		System.out.println("App running...");
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -36,9 +38,28 @@ public class Start {
 				
 			}else if(choice==2) {
 				//delete
+				System.out.println("Student id: ");
+				int id=Integer.parseInt(br.readLine());
+				if(StudentDao.deletStudent(id)) {
+					System.out.println("Deleted");
+				}else System.out.println("Error occurred ");
 				
 			}else if(choice==3) {
 				//display
+				System.out.println("Student  are: ");
+				ResultSet stu = StudentDao.displayStudent();
+				 while (stu.next()) {
+					 // `sname`, `sphone`, `scity`
+					 	System.out.println("Student id: "+stu.getString("sid"));
+					 
+						System.out.println("Student name: "+stu.getString("sname"));
+						
+						System.out.println("Student phone no.:"+stu.getString("sphone"));
+					
+						System.out.println("Student city:"+stu.getString("scity"));
+						
+						System.out.println("-------------------------------------");
+				 }
 				
 			}else if(choice==4) {
 				//exit
--- StudentManagementApp/src/com/start/student/StudentDao.java
+++ StudentManagementApp/src/com/start/student/StudentDao.java
@@ -2,24 +2,65 @@ package com.start.student;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
 
 public class StudentDao {
+
 	public static boolean addStudent(Student st) {
-		
+
 		try {
-			Connection con=ConnectionProvider.CreateCon();
-			String query="INSERT INTO `studentmanagement`.`students` ( `sname`, `sphone`, `scity`) VALUES (?,?,?);" ;
-			PreparedStatement pstmt=con.prepareStatement(query);
+			Connection con = ConnectionProvider.CreateCon();
+			String query = "INSERT INTO `studentmanagement`.`students` ( `sname`, `sphone`, `scity`) VALUES (?,?,?);";
+			PreparedStatement pstmt = con.prepareStatement(query);
 			pstmt.setString(1, st.getSname());
 			pstmt.setString(2, st.getSphone());
 			pstmt.setString(3, st.getSphone());
-			pstmt.execute();//boolean return type 
+			pstmt.execute();// boolean return type
 			return true;
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			// TODO: handle exception
+			System.out.println(e);
 		}
 		return false;
 	}
+
+	public static boolean deletStudent(int id) {
+
+		try {
+			Connection con = ConnectionProvider.CreateCon();
+			String query = "DELETE FROM `studentmanagement`.`students` WHERE (`sid` = ?);";
+			PreparedStatement pstmt = con.prepareStatement(query);
+			pstmt.setLong(1, id);
+
+			pstmt.execute();// boolean return type
+			return true;
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		return false;
+	}
+	
+	
+
+	public static ResultSet displayStudent() {
+
+		try {
+			Connection con = ConnectionProvider.CreateCon();
+			String query = "SELECT * FROM studentmanagement.students;";
+			PreparedStatement pstmt = con.prepareStatement(query);
+		
+			ResultSet stucents=pstmt.executeQuery();
+			
+			return stucents;
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+		return null;
+	}
+	
 }
